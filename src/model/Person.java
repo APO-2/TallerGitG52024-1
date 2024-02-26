2af2917842e7d99a327c1ea318c635051b569631
njse22 <njse22@gmail.com>
2024-02-26 08:09:17 -0500

    update person class

--- src/model/Person.java
+++ src/model/Person.java
@@ -10,4 +10,21 @@ public class Person{
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
 }
